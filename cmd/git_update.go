19874babdc8a85f8756ac8eaff5e4c50ecff52ab
Salvatore Spagnuolo - Bip RED <salvatore.spagnuolo@vidiemme.it>
2025-11-10 12:15:47 +0100

    implementate funzionalità di git update

--- cmd/git_update.go
+++ cmd/git_update.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"path/filepath"
+	"strings"
 
 	"github.com/SalvatoreSpagnuolo-BipRED/projman/internal/config"
 	"github.com/SalvatoreSpagnuolo-BipRED/projman/internal/executil"
@@ -25,33 +26,120 @@ dai rispettivi repository remoti.`,
 			return
 		}
 
-		// Esegui git pull per ogni progetto selezionato
-		spinner, _ := pterm.DefaultSpinner.Start("Eseguo git pull")
+		// Chiedi all'utente se vuole passare al ramo develop (solo se necessario)
+
+		// Elabora ogni progetto individualmente
 		for _, p := range cfg.SelectedProjects {
-			spinner.UpdateText("Aggiorno il progetto: " + p)
+			pterm.DefaultSection.Println("Elaborazione progetto: " + p)
 			projectPath := filepath.Join(cfg.RootOfProjects, p)
-			if err := executil.Run("git", "-C", projectPath, "pull"); err != nil {
-				pterm.Error.Println("Errore durante l'aggiornamento del progetto " + p + ": " + err.Error())
-			} else {
-				pterm.Success.Println("Progetto aggiornato: " + p)
+			hasStash := false
+			switchToDevelop := false
+
+			// Controlla il ramo corrente
+			isDevelopBranch, isDeployBranch, currBranch, err := branchInformation(projectPath)
+			if err := errorHandling("Impossibilre recuperare info sul bransh", err); err != nil {
+				continue
 			}
+
+			// Chiedi se si vuole cambiare ramo solo se non è già develop
+			if !isDevelopBranch {
+				pterm.Info.Println("Ramo corrente:", currBranch)
+				switchToDevelop, _ = pterm.DefaultInteractiveConfirm.Show("Vuoi passare tutti i progetti al ramo 'develop' prima di eseguire il pull?")
+			}
+
+			// 1. Verifica e stasha i cambiamenti non committati
+			status, err := executil.RunWithOutput("git", "-C", projectPath, "status", "--porcelain")
+			if err := errorHandling("Impossibile versificare esistenza stash", err); err != nil {
+				continue
+			}
+
+			if status != "" {
+				pterm.Info.Println("Rilevati cambiamenti non committati, eseguo stash...")
+				if err := errorHandling("Errore durante lo stash", executil.Run("git", "-C", projectPath, "stash", "push", "-m", "projman auto-stash")); err != nil {
+					continue
+				}
+
+				hasStash = true
+				pterm.Success.Println("Stash eseguito con successo")
+
+			}
+
+			// 2. Cambio ramo se richiesto
+			if switchToDevelop {
+				pterm.Info.Println("Cambio ramo a develop...")
+				if err := errorHandling("Errore durante il cambio ramo", executil.Run("git", "-C", projectPath, "checkout", "develop")); err != nil {
+					continue
+				}
+
+				pterm.Success.Println("Ramo cambiato a develop")
+				isDevelopBranch, isDeployBranch, currBranch = true, false, "develop"
+			}
+
+			// 3. Esegui git pull
+			pterm.Info.Println("Eseguo git pull...")
+			if isDevelopBranch {
+				// git pull per develop
+				if err := errorHandling("Errore durante il git pull", executil.Run("git", "-C", projectPath, "pull", "origin/develop")); err != nil {
+					continue
+				}
+				pterm.Success.Println("Git pull eseguito con successo sul ramo develop")
+			} else if isDeployBranch {
+				// git pull per deploy
+				if err := errorHandling("Errore durante il git pull", executil.Run("git", "-C", projectPath, "pull", "origin/"+currBranch)); err != nil {
+					continue
+				}
+				pterm.Success.Println("Git pull eseguito con successo sul ramo " + currBranch)
+			} else {
+				// git fetch + git merge (develop) per altri rami
+				if err := errorHandling("Errore durante il git fetch", executil.Run("git", "-C", projectPath, "fetch", "origin/develop")); err != nil {
+					continue
+				}
+				if err := errorHandling("Errore durante il git merge", executil.Run("git", "-C", projectPath, "merge", "origin/develop")); err != nil {
+					continue
+				}
+				pterm.Success.Println("Fetch e merge di develop eseguito con successo sul ramo " + currBranch)
+			}
+
+			// 4. Ripristina lo stash se era stato fatto
+			if hasStash {
+				pterm.Info.Println("Ripristino modifiche locali...")
+				if err := errorHandling("Errore durante il ripristino dello stash", executil.Run("git", "-C", projectPath, "stash", "pop")); err != nil {
+					continue
+				}
+				pterm.Success.Println("Modifiche locali ripristinate con successo")
+			}
+
+			pterm.Println() // Riga vuota per separare i progetti
 		}
 
-		spinner.Success("Git pull completato")
+		pterm.Success.Println("Operazioni completate per tutti i progetti")
 
 	},
 }
 
 func init() {
 	gitCmd.AddCommand(updateCmd)
+}
 
-	// Here you will define your flags and configuration settings.
+func branchInformation(projectPath string) (bool, bool, string, error) {
+	currentBranch, err := executil.RunWithOutput("git", "-C", projectPath, "branch", "--show-current")
+	if err != nil {
+		return false, false, "", err
+	}
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// updateCmd.PersistentFlags().String("foo", "", "A help for foo")
+	isDevelopBranch := currentBranch == "develop"
+	isDeployBranch := strings.HasPrefix(currentBranch, "deploy")
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// updateCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	return isDevelopBranch, isDeployBranch, currentBranch, nil
+
+}
+
+func errorHandling(msg string, err error) error {
+	if err != nil {
+		pterm.Warning.Printfln("Si è verificato un errore: %s, Risolvi manualmente il problema prima di continuare.", msg)
+		pterm.Error.Println(err.Error())
+		pterm.DefaultInteractiveConfirm.Show("Premi Invio per continuare con il prossimo progetto")
+		return err
+	}
+	return nil
 }
--- internal/executil/exec.go
+++ internal/executil/exec.go
@@ -3,6 +3,7 @@ package executil
 import (
 	"os"
 	"os/exec"
+	"strings"
 )
 
 func Run(name string, args ...string) error {
@@ -11,4 +12,11 @@ func Run(name string, args ...string) error {
 	cmd.Stderr = os.Stderr
 	cmd.Env = os.Environ()
 	return cmd.Run()
+}
+
+func RunWithOutput(name string, args ...string) (string, error) {
+	cmd := exec.Command(name, args...)
+	cmd.Env = os.Environ()
+	output, err := cmd.Output()
+	return strings.TrimSpace(string(output)), err
 }
